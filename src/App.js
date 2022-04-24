e8e37fb88c6dbc9e798ab3ad9a1546f4cb460b38
Fathurrachman D. Saputra <namasayafathur@gmail.com>
2022-04-24 19:31:09 +0700

    Release 2: Show now playing movies on home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
 import './App.css';
+import axios from "axios";
 import Navbar from "./components/Navbar.js"
 import Home from "./pages/Home.js"
 import Favorites from './pages/Favorites.js';
 
 class App extends React.Component {
   state = {
-    activePage: 'Home'
+    activePage: 'Home',
+    posts: [],
+    isLoading: false
   };
+
+  componentDidMount(){
+    this.setState({
+      isLoading: true
+    })
+
+    axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=fe9c2107e7e76afb20fd484f3d893e7f&language=en-US&page=1')
+      .then(data => {
+        this.setState({
+          posts: data.data.results
+        })
+      })
+      .catch(err => {
+        console.log(err, 'ini error dari catch');
+      })
+      .finally(() => {
+        this.setState({
+          isLoading: false
+        })
+      })
+  }
 
   render() {
     return (
-      <div className='background-color'>
+      <div className='background-color'>      
+      
         <Navbar currentPage={(page) => {
           this.setState({
             activePage: page,
           });
         }}/>
-        { this.state.activePage === 'Home' && <Home />}
-        { this.state.activePage === 'Favorites' && <Favorites />}
+
+        <div>
+          {this.state.isLoading ? (<p className='text-center'>Loading...</p>) : (
+            <>
+              {this.state.activePage === 'Home' && <Home posts={this.state.posts}/>}
+              {this.state.activePage === 'Favorites' && <Favorites />}
+            </>
+          )}
+        </div>
       </div>
     );
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,72 +1,21 @@
 import React, { Component } from "react";
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col, Image, Card, Button } from 'react-bootstrap';
 import "../styles/Home.css";
 
 export default class Home extends Component {
     render() {
         return (
-            <Container fluid>
-                <Row xs={2} md={3} lg={6} className='my-3'>
-                <Col>
-                    <Image fluid src="https://image.tmdb.org/t/p/w500//74xTEgt7R36Fpooo50r9T25onhq.jpg"></Image>
-                </Col>
-                <Col>
-                    <Image fluid src="https://image.tmdb.org/t/p/w500//qsdjk9oAKSQMWs0Vt5Pyfh6O4GZ.jpg"></Image>
-                </Col>
-                <Col>
-                    <Image fluid src="https://image.tmdb.org/t/p/w500//abPQVYyNfVuGoFUfGVhlNecu0QG.jpg"></Image>
-                </Col>
-                <Col>
-                    <Image fluid src="https://image.tmdb.org/t/p/w500//6nhwr1LCozBiIN47b8oBEomOADm.jpg"></Image>
-                </Col>
-                <Col>
-                    <Image fluid src="https://image.tmdb.org/t/p/w500//sqLowacltbZLoCa4KYye64RvvdQ.jpg"></Image>
-                </Col>
-                <Col>
-                    <Image fluid src="https://image.tmdb.org/t/p/w500//3pTwMUEavTzVOh6yLN0aEwR7uSy.jpg"></Image>
-                </Col>
-                </Row>
-                <Row xs={2} md={3} lg={6} className='my-3'>
-                <Col>
-                    <Image fluid src="https://image.tmdb.org/t/p/w500//jrgifaYeUtTnaH7NF5Drkgjg2MB.jpg"></Image>
-                </Col>
-                <Col>
-                    <Image fluid src="https://image.tmdb.org/t/p/w500//ejXBuNLvK4kZ7YcqeKqUWnCxdJq.jpg"></Image>
-                </Col>
-                <Col>
-                    <Image fluid src="https://image.tmdb.org/t/p/w500//yD9RhgIVydQNBK7OLEbCWYcWMUd.jpg"></Image>
-                </Col>
-                <Col>
-                    <Image fluid src="https://image.tmdb.org/t/p/w500//moLnqJmZ00i2opS0bzCVcaGC0iI.jpg"></Image>
-                </Col>
-                <Col>
-                    <Image fluid src="https://image.tmdb.org/t/p/w500//8eOgpzYUV4PxbDl64vrWpWTEAqb.jpg"></Image>
-                </Col>
-                <Col>
-                    <Image fluid src="https://image.tmdb.org/t/p/w500//e4koV8iC2cCM57bqUnEnIL2a2zH.jpg"></Image>
-                </Col>
-                </Row>
-                <Row xs={2} md={3} lg={6} className='my-3'>
-                <Col>
-                    <Image fluid src="https://image.tmdb.org/t/p/w500//nmD0vZk8jQg1Suhs0iv9cj0xdtZ.jpg"></Image>
-                </Col>
-                <Col>
-                    <Image fluid src="https://image.tmdb.org/t/p/w500//aSRvK4kLJORBrVdlFn2wrGx8XPv.jpg"></Image>
-                </Col>
-                <Col>
-                    <Image fluid src="https://image.tmdb.org/t/p/w500//5vePsmbekQ1ysXS6f0lhzgCwvc4.jpg"></Image>
-                </Col>
-                <Col>
-                    <Image fluid src="https://image.tmdb.org/t/p/w500//wxopaacKXHZUq92gVco2q9Q7fAE.jpg"></Image>
-                </Col>
-                <Col>
-                    <Image fluid src="https://image.tmdb.org/t/p/w500//sUw081I0k5JWL6NpTaDRjMpLKwr.jpg"></Image>
-                </Col>
-                <Col>
-                    <Image fluid src="https://image.tmdb.org/t/p/w500//1gTnopPJgauN46CYGobPyZCZQTn.jpg"></Image>
-                </Col>
-                </Row>
-            </Container>
+            <div className="d-flex justify-content-around flex-wrap">
+                {this.props.posts && this.props.posts.map((el, i) => (
+                    <Card className="m-2" style={{ width: '15rem' }} key={i}>
+                        <Card.Img variant="top" src={"https://image.tmdb.org/t/p/w500" + el.poster_path} />
+                        {/* <Card.Body>
+                            <Card.Title className="text-dark text-center">{el.title}</Card.Title>
+                            <Button className="w-100" variant="success">Detail</Button>
+                        </Card.Body> */}
+                    </Card>
+                ))}
+            </div>
         );
     }
 }
